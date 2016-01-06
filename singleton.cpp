dcc5c9dfdd2d5f0f41e6f620e9c7a3597650d43c
fei zhou <kobefaith@163.com>
2016-01-06 23:36:19 +0800

    add the singleton.cpp

--- singleton.cpp
+++ singleton.cpp
@@ -31,3 +31,40 @@ private:
 	Singleton(const Singleton& rhs){}
 	singleton& operator = (const Singleton& rhs){}
 };
+template<typename T>
+class Singleton
+{
+public:
+	static T& Instance(){
+		if(m_pInstance == NULL)
+		{
+			Lock lock;
+			if(m_pInstance == NULL)
+			{
+				m_pInstance = new T();
+				atexit(Destory);
+
+			}
+			return *m_pInstance;
+		}
+		return *m_pInstance;
+
+	}
+protected:
+	Singleton(void){}
+	~Singleton(void){}
+private:
+	Singleton(const Singleton& rhs){}
+	Singleton& operator = (const Singleton& rhs){}
+
+	void Destory()
+	{
+		if(m_pInstance != NULL)
+			delete m_pInstance;
+		m_pInstance = NULL;
+	}
+	static T* volatile m_pInstance;
+
+};
+template<typename T>
+T* singleton<T>::m_pInstance = NULL;

2f5b399a493a6a30286b738d0c4be2001651112c
fei zhou <kobefaith@163.com>
2016-01-06 23:20:39 +0800

    add the singleton.cpp

--- singleton.cpp
+++ singleton.cpp
@@ -14,3 +14,20 @@ private:
 	Singleton(){};
 
 };
+
+template <typename T>
+class Singleton
+{
+public:
+	static T& Instance()
+	{
+		static T s_Instance;
+		return s_Instance;
+	}
+protected:
+	Singleton(void){}
+	~Singleton(void){}
+private:
+	Singleton(const Singleton& rhs){}
+	singleton& operator = (const Singleton& rhs){}
+};

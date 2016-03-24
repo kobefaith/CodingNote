dfd026663534c39372ece1b0f2afb0b5536c8a73
fei zhou <kobefaith@163.com>
2016-03-24 22:38:14 +0800

    add th edp.txt

--- Design pattern/DP.cpp
+++ Design pattern/DP.cpp
@@ -102,7 +102,64 @@ class DecoratePhoneB:public DecoratePhone
     private:
     void AddDecorate(){cout<<"屏幕贴膜"<<endl;}
 }
+#include<iostream>
+#include<list>
+#include<string>
+using namespace std;
 
+class Observer
+{
+public:
+    Observer(){}
+    virtual ~Observer(){}
+    virtual void Update(){}
+};
+
+class Blog
+{
+public:
+    Blog(){}
+    virtual ~Blog(){}
+    void Attach(Observer *observer){ m_observers.push_back(observer); }
+    void Remove(Observer *observer){ m_observers.remove(observer); }
+    void Notify(){
+        list<Observer*>::iterator iter = m_observers.begin();
+        for (; iter != m_observers.end(); iter++){
+            (*iter)->Update();
+        }
+    }
+    virtual void SetStatus(string s){ m_status = s; }
+    virtual string GetStatus(){ return m_status; }
+private:
+    list<Observer*> m_observers;
+protected:
+    string m_status;
+
+};
+
+class BlogCSDN :public Blog
+{
+private:
+    string m_name;
+public:
+    BlogCSDN(string name) :m_name(name){}
+    ~BlogCSDN(){}
+    void SetStatus(string s){ m_status = "CSDN:" + m_name + s; }
+    string GetStatus(){ return m_status; }
+};
+class ObserverBlog :public Observer
+{
+private:
+    string m_name;
+    Blog *m_blog;
+public:
+    ObserverBlog(string name, Blog *blog) :m_name(name), m_blog(blog){}
+    ~ObserverBlog(){}
+    void Update(){
+        string status = m_blog->GetStatus();
+        cout << m_name << "----------" << status << endl;
+    }
+};
 
 
 

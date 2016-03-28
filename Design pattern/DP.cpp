41f9f6df3910deec12fe23d99bd49f58ebccbdb6
fei zhou <kobefaith@163.com>
2016-03-28 23:19:05 +0800

    add the change

--- Design pattern/DP.cpp
+++ Design pattern/DP.cpp
@@ -160,6 +160,46 @@ public:
         cout << m_name << "----------" << status << endl;
     }
 };
+funtion Circle(){
+    createjs.Shape.call(this);
+    this.setCircleType = function(type){
+        this._circleType = type;
+        switch(type){
+            case 1:
+                this.setColor("#cccccc");
+                break;
+            case 2:
+                this.setColor("#ff6600");
+                break;
+            case 3:
+                this.setColor("#0000ff");
+                break;                
+        }
+    }
+    this.setColor = function(colorString){
+        this.graphics.beginFill(colorString);
+        this.graphics.drawCircle(0,0,25);
+        this.graphics.endFill();
+    }
+    this.getCircleType = function(){
+        return this._circleType;
+    }
+    this.setCircleType(1);
+}
+Circle.prototype = new createjs.Shape();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
